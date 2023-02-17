fda099f73ed3dee5528f2f20d1f135f449259954
Tariyel <islami.tariyel@gmail.com>
2023-02-17 18:05:03 +0400

    Initial commit

---
+++ app/src/main/java/com/zeroone/jetpackcompose_pokedex/PokedexApp.kt
@@ -0,0 +1,9 @@
+package com.zeroone.jetpackcompose_pokedex
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class PokedexApp : Application(){
+
+}
---
+++ app/src/main/java/com/zeroone/jetpackcompose_pokedex/domain/model/Response.kt
@@ -0,0 +1,7 @@
+package com.zeroone.jetpackcompose_pokedex.domain.model
+
+sealed class Response<out T>{
+    object Loading: Response<Nothing>()
+    data class Success<out T>(val data: T): Response<T>()
+    data class Error(val message: String): Response<Nothing>()
+}
